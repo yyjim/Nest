5ce703f5583873013551008f3af58733efb6de64
yyjim <yy.jim731@gmail.com>
2025-07-31 19:43:20 +0800

    fix: Core Data cannot be processed by a Copy Bundle Resources build phase warnings (disable codegen for CoreData model)

---
+++ Sources/Nest/Database/CoreData/Asset+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  Asset+CoreDataProperties.swift
+//  Nest
+//
+//  Created by Jim Wang on 2025/7/31.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Asset {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Asset> {
+        return NSFetchRequest<Asset>(entityName: "Asset")
+    }
+
+    @NSManaged public var createdAt: Date?
+    @NSManaged public var fileSize: Int64
+    @NSManaged public var id: String?
+    @NSManaged public var metadata: String?
+    @NSManaged public var modifiedAt: Date?
+    @NSManaged public var type: String?
+
+}
