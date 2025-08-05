b69bf04052bea2ee555d98d9eb543ee8a4da0e39
Jim Wang <yy.jim731@gmail.com>
2025-08-05 08:58:53 +0800

    fix: Core Data cannot be processed by a Copy Bundle Resources build phase warnings (#12)
    * fix: Core Data cannot be processed by a Copy Bundle Resources build phase warnings (disable codegen for CoreData model)
    
    * refactor: mark id, type, createdAt non-optional

---
+++ Sources/Nest/Database/CoreData/Asset+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Asset+CoreDataClass.swift
+//  Nest
+//
+//  Created by Jim Wang on 2025/7/31.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Asset)
+public class Asset: NSManagedObject {
+
+}
